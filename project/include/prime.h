020db7c8104e26ebb483dfa7642307fa1f84bfaf
Vladislav Melnichuk <vs.melnichuk09@gmail.com>
2021-11-01 01:54:37 +0300

    2

--- project/include/prime.h
+++ project/include/prime.h
@@ -1,14 +1,10 @@
 #ifndef PROJECT_INCLUDE_PRIME_H_
 #define PROJECT_INCLUDE_PRIME_H_
 
+#include <stdbool.h>
 #include <stddef.h>
 
-/* Можно было подключить <stdbool.h> и использовать _Bool, но в памяти занимает,
- * как int, работает только приведение типов как bool
- * аналог (!!a) нормировка
- * поэтому будем использовать size_t
- * */
-size_t custom_prime(long long num);
+bool custom_prime(int num);
 
 #endif  // PROJECT_INCLUDE_PRIME_H_
 
